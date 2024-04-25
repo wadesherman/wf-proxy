de73451d3603106072cb71b0085d12e819c74819
Wade Sherman <wadebsherman@gmail.com>
2024-04-25 09:34:23 -0700

    update forecast every 30 min

--- src/main/scala/com/casadeoso/Main.scala
+++ src/main/scala/com/casadeoso/Main.scala
@@ -84,7 +84,7 @@ object Main extends IOApp with StrictLogging {
                 .flatMap {
                   case Left(value) => IO(Left(value))
                   case Right(f) =>
-                    Cache.put(f, forecastCache, 59.minutes).map {
+                    Cache.put(f, forecastCache, 29.minutes).map {
                       case Some(f) => Right(f)
                       case None => {
                         logger.warn(f"Error writing forecast to cache")
